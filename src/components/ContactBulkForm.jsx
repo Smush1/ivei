5e8cf3e30a889c93ccbc1f02a7d83c4c1a4a1055
sumeshk1 <sumesh1712@gmail.com>
2025-11-18 15:08:17 +0530

    content update

--- src/components/ContactBulkForm.jsx
+++ src/components/ContactBulkForm.jsx
@@ -7,7 +7,7 @@ function ContactBulkForm() {
       <div className="bulk-form-overlay">
       <div className="row justify-content-center">
         <div className="col-lg-5 text-btn-container text-center col-md-12">
-          <h3 className="bulk-left-heading">Customized Corporate Gifts & Business Gifting Solutions with<p className="bulk-left-custom-heading">IVEI</p></h3>
+          <h3 className="bulk-left-heading">Customized Corporate Gifts & Business Gifting Solutions from<p className="bulk-left-custom-heading">IVEI</p></h3>
           {/* <p className="bulk-left-para">Stock up on Stonekraft’s quality products and enjoy guaranteed discounts on large quantity purchases.</p> */}
           {/* <a className="btn btn-primary bulk-buy-btn px-4 py-3 mt-2" href="#" rel="noreferrer" role="button">Contact for bulk buying</a> */}
 
